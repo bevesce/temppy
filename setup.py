64e3728133f3242eccebe151b4c857c66e54482b
Piotr Wilczynski <wilczynski@a3mobile.com>
2016-05-04 14:10:29 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'temppy',
+  packages = ['temppy'],
+  version = '1.0',
+  description = 'Very simple and not that powerful template engine based on eval',
+  author = 'Piotr Wilczynski',
+  author_email = 'wilczynski.pi@gmail.com',
+  url = 'https://github.com/bevesce/temppy',
+  # download_url = '',
+  keywords = ['template'],
+  classifiers = [],
+)
